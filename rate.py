ba2680f4f7ebecb7f0d623f2fc631027c128859f
Yanbo Du <yanbodu@users.noreply.github.com>
2017-10-20 12:28:14 -0400

    Add files via upload

--- rate.py
+++ rate.py
@@ -19,6 +19,8 @@ if __name__ == '__main__':
 
     browser.find_element_by_xpath('//*[@id="cookie_notice"]/a[1]').click()
 
+    browser.refresh()
+
     # click dropdown button
     browser.find_element_by_xpath('//*[@id="mainContent"]/div[1]/div/div[3]/div/div/span').click()
 
@@ -30,7 +32,6 @@ if __name__ == '__main__':
     LoadMore = browser.find_element_by_xpath('//*[@id="mainContent"]/div[1]/div/div[5]/div/div[1]')
     browser.execute_script("arguments[0].scrollIntoView();", LoadMore)
 
-    browser.find_element_by_xpath('//*[@id="spout-header-close"]').click()
     if(LoadMore.is_displayed()):
         LoadMore.click()
 
